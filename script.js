fbf3acb1cda48c2a456bf205f217868ee03d8b99
Matt Morgante <m.morgante@telegraafmedia.nl>
2017-01-16 13:12:28 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Technical requirements: 
+// Based on input, display two different boxes 
+// jQuery UI Datepicker 
+// Save date as a variable 
+// Function to Calculate days since date was picked 
+// Takes initial date as parameter, returns an integer 
+// Multiply days by an array of constants, save the result as a dynamic array 
+// Loop through foreach that shows a styled box for each array value 
+// On document load, script runs that increases each (CONST/86400) seconds
+// Where 86400 is number of seconds in a day  
+
